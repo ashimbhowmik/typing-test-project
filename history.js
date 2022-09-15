55d07bdf53072ba63db249056ac0d4fec8e6c9d5
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-09-15 18:41:54 +0600

    [delete one test item]

--- history.js
+++ history.js
@@ -16,7 +16,8 @@ function addHistory(questionText, timeTaken, errorCount) {
   histories.appendChild(newRow);
 
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-  previousTests.push({ questionText, timeTaken, errorCount });
+  let id = Math.round(Math.random() * 10000);
+  previousTests.push({ questionText, timeTaken, errorCount, id });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
 
   displayHistory();
@@ -31,15 +32,27 @@ function displayHistory() {
     newRow.classList.add("card");
 
     newRow.innerHTML = `
-  <h3 class="margine-fixed">${test.questionText}</h3>
-  <p class="margine-fixed">You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+    <div id="${test.id}">
+    <h3 class="margine-fixed">${test.questionText}</h3>
+    <p class="margine-fixed">You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+      <div class="card-design ">
+      <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+      <i id="remove-size" class="fa-solid fa-trash-can remove-size" onclick="removeStorage(${test.id})"></i>
+      </div>
+    </div>
   `;
 
     histories.appendChild(newRow);
   });
 }
+const removeStorage = (id) => {
+  let data = JSON.parse(localStorage.getItem("testHistory")) || [];
+  let newData = data.filter((x) => x.id !== id);
+  localStorage.setItem("testHistory", JSON.stringify(newData));
+  window.location.reload()
+};
 
 document.getElementById('open-blog').addEventListener('click', function () {
   window.location.href = 'blog.html';
 })
+
